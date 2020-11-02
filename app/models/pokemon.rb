f32315dafa3f687a4f26928d954e9a9a2dae0895
eviry-ishikawa <ishikawa@eviry.com>
2020-11-02 16:32:23 +0900

    テスト

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,7 @@
 class Pokemon < ApplicationRecord
+    def show_jobs
+      Sidekiq::Workers.new.each do |process_id, thread_id, job|
+        puts "#{process_id}, #{thread_id}, #{job}"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: "/sidekiq"
   resources :pokemons, only: [:index]
   post 'pokemons/upload', to:'pokemons#upload'
   post 'pokemons/destroy_all', to: 'pokemons#destroy_all'
